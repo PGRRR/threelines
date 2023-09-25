f6812e03e01c62fd75fcd484fab4110a4bac5fce
PGRRR <pgrrr119@gmail.com>
2023-09-25 23:34:48 +0900

    config: add ddl sql script - DDL 스크립트 추가

---
+++ src/main/resources/script/threelines_ddl_v1.0.sql
@@ -0,0 +1,34 @@
+CREATE TABLE `MEMBER` (
+                          `MEMBER_NO` INT AUTO_INCREMENT PRIMARY KEY,
+                          `MEMBER_EMAIL` VARCHAR(255) NULL,
+                          `MEMBER_NM` VARCHAR(255) NULL,
+                          `MEMBER_PW` VARCHAR(255) NULL,
+                          `REG_ID` INT NULL,
+                          `REG_TIME` DATETIME NULL,
+                          `ALTER_ID` INT NULL,
+                          `ALTER_TIME` DATETIME NULL
+);
+
+CREATE TABLE `SUBSCRIPTION` (
+                                `SUB_NO` INT AUTO_INCREMENT PRIMARY KEY,
+                                `SUB_TITLE` VARCHAR(255) NULL,
+                                `SUB_TYPE` VARCHAR(255) NULL,
+                                `MEMBER_NO` INT NOT NULL,
+                                `REG_ID` INT NULL,
+                                `REG_TIME` DATETIME NULL,
+                                `ALTER_ID` INT NULL,
+                                `ALTER_TIME` DATETIME NULL
+);
+
+CREATE TABLE `CONTENT` (
+                           `CONTENT_NO` INT AUTO_INCREMENT PRIMARY KEY,
+                           `CONTENT_TITLE` VARCHAR(255) NULL,
+                           `CONTENT_ADDR` VARCHAR(255) NULL,
+                           `CONTENT_SUMMARY` TEXT NULL,
+                           `CONTENT_TYPE` VARCHAR(255) NULL,
+                           `SUB_NO` INT NOT NULL,
+                           `REG_ID` INT NULL,
+                           `REG_TIME` DATETIME NULL,
+                           `ALTER_ID` INT NULL,
+                           `ALTER_TIME` DATETIME NULL
+);
